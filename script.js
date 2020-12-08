d502aa4acca42974c90f606bd8dc93e5597a9420
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-12-08 07:47:01 +0100

    Add functionality to double everyones wealth

--- script.js
+++ script.js
@@ -45,4 +45,13 @@ function formatMoney(number) {
     return '€' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
 }
 
+function doubleMoney() {
+    data = data.map((user) => {
+        return { ...user, money: user.money * 2 }
+    })
+
+    updateDOM();
+}
+
 addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
